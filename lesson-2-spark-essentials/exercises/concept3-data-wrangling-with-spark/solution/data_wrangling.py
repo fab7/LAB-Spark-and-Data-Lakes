cb3bb63bbfb143873a0aa7609c4e66b47f964a24
Francois <fab@zurich.ibm.com>
2023-06-19 13:27:48 +0200

    Commenting the code

--- lesson-2-spark-essentials/exercises/concept3-data-wrangling-with-spark/solution/data_wrangling.py
+++ lesson-2-spark-essentials/exercises/concept3-data-wrangling-with-spark/solution/data_wrangling.py
@@ -1,8 +1,10 @@
-# # Data Wrangling with Spark
+##############################################################################
 # 
-# This is the code used in the previous screencast. Run each code cell to understand what the code does and how it works.
+#  DATA WRANGLING with SPARK
 # 
-# These first three cells import libraries, instantiate a SparkSession, and then read in the data set
+#    Run each code cell to understand what the code does and how it works.
+#
+##############################################################################
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
@@ -16,58 +18,62 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#==========================================================
+# Instantiate a SparkSession and then read in the data set
+#==========================================================
 spark = SparkSession \
     .builder \
     .appName("Wrangling Data") \
     .getOrCreate()
 
-path = "./lesson-2-spark-essentials/exercises/data/sparkify_log_small.json"
+path = "../../data/sparkify_log_small.json"
 user_log_df = spark.read.json(path)
 
+print("\n#==========================================================")
+print("# Data Exploration - Let's explore the data set.           ")
+print("#==========================================================")
 
-# # Data Exploration 
-# 
-# # Explore the data set.
-
-
-# View 5 records 
+print("\n# take(5) - These are the 5 first records") 
 print(
     user_log_df.take(5)
 )
-# Print the schema
+
+print("\n# printSchema() - This the schema inferred by Spark")
 user_log_df.printSchema()
 
-# Describe the dataframe
+print("\n# show() - Describe the dataframe")
 user_log_df.describe().show()
 
-# Describe the statistics for the song length column
+print("\n# describe().show)() - Describe the statistics for the song length column")
 user_log_df.describe("length").show()
 
-# Count the rows in the dataframe
+print("\n# count() - Count the rows in the dataframe")
 print(
     user_log_df.count()
 )    
 
-# Select the page column, drop the duplicates, and sort by page
+print("\n# select().dropDuplicates().sort().show() - Select the page column, drop the duplicates, and sort by page")
 user_log_df.select("page").dropDuplicates().sort("page").show()
 
-# Select data for all pages where userId is 1046
+print("\n# select().where().show() - Select data for all pages where userId is 1046")
 user_log_df.select(["userId", "firstname", "page", "song"]) \
     .where(user_log_df.userId == "1046") \
     .show()
 
-
-# # Calculating Statistics by Hour
+print("\n# Calculating Statistics by Hour")
+# NOTE: 'udf()' creates a user defined function (UDF).
 get_hour = udf(lambda x: datetime.datetime.fromtimestamp(x / 1000.0). hour)
 
+# Now that we have created that UDF, we can use it as a new column
+# that we name 'hour'.
 user_log_df = user_log_df.withColumn("hour", get_hour(user_log_df.ts))
 
+print("\n# head() - Let's get the first row to see if the new column was added")
 print(
-    # Get the first row
     user_log_df.head(1)
 )
 
-# Select just the NextSong page
+print("\n# filter().groupby().count().orderBy() - Select just the NextSong page")
 songs_in_hour_df = user_log_df.filter(user_log_df.page == "NextSong") \
     .groupby(user_log_df.hour) \
     .count() \
@@ -75,6 +81,7 @@ songs_in_hour_df = user_log_df.filter(user_log_df.page == "NextSong") \
 
 songs_in_hour_df.show()
 
+print("\n# toPAndas() - For scatter plot")
 songs_in_hour_pd = songs_in_hour_df.toPandas()
 songs_in_hour_pd.hour = pd.to_numeric(songs_in_hour_pd.hour)
 
@@ -85,36 +92,38 @@ plt.xlabel("Hour")
 plt.ylabel("Songs played")
 plt.show()
 
-
-# # Drop Rows with Missing Values
-# 
-# As you'll see, it turns out there are no missing values in the userID or session columns. But there are userID values that are empty strings.
-# how = 'any' or 'all'. If 'any', drop a row if it contains any nulls. If 'all', drop a row only if all its values are null.
+print("\n# dropna() - Drop Rows with Missing Values")
+# As you'll see, it turns out there are no missing values in the userID or 
+# session columns. But there are userID values that are empty strings.
+# how = 'any' or 'all'. 
+#  - If 'any', drop a row if it contains any nulls. 
+#  - If 'all', drop a row only if all its values are null.
 # subset = list of columns to consider
 user_log_valid_df = user_log_df.dropna(how = "any", subset = ["userId", "sessionId"])
 
-# How many are there now that we dropped rows with null userId or sessionId?
-print(
-user_log_valid_df.count()
-)
-
-# select all unique user ids into a dataframe
-user_log_df.select("userId") \
-    .dropDuplicates() \
-    .sort("userId").show()
-
-# Select only data for where the userId column isn't an empty string (different from null)
-user_log_valid_df = user_log_valid_df.filter(user_log_valid_df["userId"] != "")
-
-# Notice the count has dropped after dropping rows with empty userId
+print("\n # How many are there now that we dropped rows with null userId or sessionId?")
 print(
     user_log_valid_df.count()
 )
 
-# # Users Downgrade Their Accounts
-# 
-# Find when users downgrade their accounts and then show those log entries. 
+print("\n# select().dropDuplicates().sort() - Select all unique user IDs into a dataframe")
+user_log_df.select("userId") \
+    .dropDuplicates() \
+    .sort("userId").show()
 
+print("\n# filter() - Select only data for where the userId column isn't an empty string (different from null)")
+user_log_valid_df = user_log_valid_df.filter(user_log_valid_df["userId"] != "")
+
+print("\n# Notice the count has dropped after dropping rows with empty userId")
+print(
+    user_log_valid_df.count()
+)
+
+print("\n#==========================================================")
+print("# Find users who downgraded their accounts                 ")
+print("#==========================================================")
+
+print("\n# Users who downgraded their accounts")
 user_log_valid_df.filter("page = 'Submit Downgrade'") \
     .show()
 
@@ -125,7 +134,7 @@ user_log_df.select(["userId", "firstname", "page", "level", "song"]) \
 # Create a user defined function to return a 1 if the record contains a downgrade
 flag_downgrade_event = udf(lambda x: 1 if x == "Submit Downgrade" else 0, IntegerType())
 
-# Select data including the user defined function
+print("\n# Add a column that flags the downgraded accounts")
 user_log_valid_df = user_log_valid_df \
     .withColumn("downgraded", flag_downgrade_event("page"))
 
@@ -135,21 +144,23 @@ print(
 
 from pyspark.sql import Window
 
-# Partition by user id
-# Then use a window function and cumulative sum to distinguish each user's data as either pre or post downgrade events.
+print("\n# partitionBy().orderBy().rangeBetween() - Partition by user id")
+# This uses a window function and cumulative sum to distinguish each user's 
+# data as either pre or post downgrade events.
 windowval = Window.partitionBy("userId") \
     .orderBy(desc("ts")) \
     .rangeBetween(Window.unboundedPreceding, 0)
 
-# Fsum is a cumulative sum over a window - in this case a window showing all events for a user
-# Add a column called phase, 0 if the user hasn't downgraded yet, 1 if they have
+print("\n# Fsum() - Cumulative sum over a window showing all events for a user")
+# Add a column called 'phase', with '0' if the user hasn't downgraded yet, 
+# or '1' if they have
 user_log_valid_df = user_log_valid_df \
     .withColumn("phase", Fsum("downgraded") \
     .over(windowval))
 
 user_log_valid_df.show()    
 
-# Show the phases for user 1138 
+print("\n# Show the phases for user 1138") 
 user_log_valid_df \
     .select(["userId", "firstname", "ts", "page", "level", "phase"]) \
     .where(user_log_df.userId == "1138") \
