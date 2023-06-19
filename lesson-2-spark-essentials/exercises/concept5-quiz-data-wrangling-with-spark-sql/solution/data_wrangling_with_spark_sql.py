346d941dce4b21db6a34de9c2e3e54c7214a9fde
Francois <fab@zurich.ibm.com>
2023-06-19 16:30:05 +0200

    Adds comments to the code

--- lesson-2-spark-essentials/exercises/concept5-quiz-data-wrangling-with-spark-sql/solution/data_wrangling_with_spark_sql.py
+++ lesson-2-spark-essentials/exercises/concept5-quiz-data-wrangling-with-spark-sql/solution/data_wrangling_with_spark_sql.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Spark SQL Examples
-# 
-
-
+##############################################################################
+#
+# Spark SQL Examples
+#
+############################################################################## 
 from pyspark.sql import SparkSession
-
 import datetime
 
 spark = SparkSession \
@@ -15,21 +15,23 @@ spark = SparkSession \
     .getOrCreate()
 
 
-path = "./lesson-2-spark-essentials/exercises/data/sparkify_log_small_2.json"
+path = "../../data/sparkify_log_small_2.json"
 user_log_df = spark.read.json(path)
 
 user_log_df.take(1)
+user_log_df.show(10)
 
 user_log_df.printSchema()
 
-
-# # Create a View And Run Queries
-# 
-# The code below creates a temporary view against which you can run SQL queries.
-
+##############################################################################
+#
+# Create a View And Run Queries
+#   A temporary view is used to run SQL queries against.
+#
+##############################################################################
 user_log_df.createOrReplaceTempView("user_log_table")
 
-
+print("# SELECT * FROM user_log_table LIMIT 2")
 spark.sql('''
           SELECT * 
           FROM user_log_table 
@@ -37,12 +39,14 @@ spark.sql('''
           '''
           ).show()
 
+print("# SELECT COUNT(*) FROM user_log_table")
 spark.sql('''
           SELECT COUNT(*) 
           FROM user_log_table 
           '''
           ).show()
 
+print("# SELECT userID, firstname, page, song FROM user_log_table WHERE userID == '1046' ")
 spark.sql('''
           SELECT userID, firstname, page, song
           FROM user_log_table 
@@ -50,6 +54,7 @@ spark.sql('''
           '''
           ).show()
 
+print("# SELECT DISTINCT page FROM user_log_table ORDER BY page ASC")
 spark.sql('''
           SELECT DISTINCT page
           FROM user_log_table 
@@ -57,17 +62,22 @@ spark.sql('''
           '''
           ).show()
 
-
-# # User Defined Functions
-
+##############################################################################
+#
+# User Defined Functions
+#
+##############################################################################
 spark.udf.register("get_hour", lambda x: int(datetime.datetime.fromtimestamp(x / 1000.0).hour))
 
+print("# SELECT *, get_hour(ts) AS hour FROM user_log_table LIMIT 1")
 spark.sql('''
           SELECT *, get_hour(ts) AS hour
           FROM user_log_table 
           LIMIT 1
           '''
           ).show()
+
+print("# SELECT get_hour(ts) AS hour, COUNT(*) as plays_per_hour FROM user_log_table WHERE page = \'NextSong\' GROUP BY hour ORDER BY cast(hour as int) ASC")
 
 songs_in_hour_df = spark.sql('''
           SELECT get_hour(ts) AS hour, COUNT(*) as plays_per_hour
@@ -80,10 +90,12 @@ songs_in_hour_df = spark.sql('''
 
 songs_in_hour_df.show()
 
-# # Converting Results to Pandas
-
+##############################################################################
+#
+# Converting Results to Pandas
+#
+##############################################################################
+print("# Converting Results to Pandas")
 songs_in_hour_pd = songs_in_hour_df.toPandas()
-
-
 print(songs_in_hour_pd)
 
